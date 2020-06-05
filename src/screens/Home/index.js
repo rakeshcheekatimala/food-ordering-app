5b1235aa3e22463203ca2281d70a3e541598f5fd
Rakesh Cheekatimala <rakesh.cheekatimala1@singtel.com>
2020-06-05 22:21:13 +0800

    added the default load all restaurants, search a restaruant by name

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
-import { Header } from './../../components';
+import { Header, RestaurantCard } from './../../components';
+import { filterRestaurantsByName, getAllRestaurants } from './../../api';
+import { Grid, Container, withStyles } from '@material-ui/core';
+
+const useStyles = (theme) => ({
+  root: {
+    marginTop: '2rem',
+  },
+});
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      restaurants: [],
+    };
+  }
+
+  componentDidMount() {
+    this.loadAllRestaurants();
+  }
+
+  loadAllRestaurants = async () => {
+    let {
+      data: { restaurants },
+    } = await getAllRestaurants();
+    this.setState({
+      restaurants,
+    });
+  };
+  onChangeHandler = async (e) => {
+    let {
+      data: { restaurants },
+    } = await filterRestaurantsByName(e.target.value);
+    this.setState({
+      restaurants,
+    });
+  };
+
   render() {
-    return <Header />;
+    const { classes } = this.props;
+
+    return (
+      <>
+        <Header onChangeHandler={this.onChangeHandler.bind(this)} />
+        <Container className={classes.root}>
+          <Grid container spacing={3}>
+            {this.state.restaurants.map((item) => {
+              return (
+                <Grid item xs={12} md={3} key={item.id}>
+                  <RestaurantCard key={item.id} restaurant={item} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
+      </>
+    );
   }
 }
 
-export default Home;
+export default withStyles(useStyles)(Home);
