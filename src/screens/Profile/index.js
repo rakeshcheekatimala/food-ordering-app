8d7fc04202c04ea821632437fde58fb6c1f24b7c
Rakesh Cheekatimala <rakesh.cheekatimala1@singtel.com>
2020-06-06 20:33:58 +0800

    profile component

---
+++ src/screens/Profile/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        Inside Profile Page
+      </div>
+    );
+  }
+}
+
+export default Profile;
