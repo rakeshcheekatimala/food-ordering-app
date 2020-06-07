654d10ba40228c29645c55b922babeb8ddae988a
sowndharya456 <sowndharyar12@gmail.com>
2020-06-07 13:34:44 +0530

    part 3 restaurant details component

--- src/screens/RestaurantDetail/index.js
+++ src/screens/RestaurantDetail/index.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import {getRestaurantById} from '../../api';
+import  RestaurantDetailsCard from '../../components/RestaurantDetailsCard';
+import HeaderLayout from '../HeaderLayout';
 
 class RestaurantDetail extends Component {
+  constructor(){
+    super();
+    this.state={
+      restaurant : {}
+    }
+  }
+
+  componentDidMount(){
+    this.GetRestaurantById();
+ 
+  }
+
+   GetRestaurantById = async()=>{
+    let results = await getRestaurantById('246165d2-a238-11e8-9077-720006ceb890');
+    console.log(results);
+    this.setState({restaurant : results.data})
+    
+  }
+
   render() {
-    return <h1>Inside RestaurantDetail Screen</h1>;
+    return <div><HeaderLayout />
+    {this.state.restaurant &&
+    <RestaurantDetailsCard restaurant= {this.state.restaurant}></RestaurantDetailsCard>
+    }
+        
+    </div>
+    
   }
 }
 
