12330e1d356275c01cc7f7913e15f38b47987a53
Rakesh Cheekatimala <rakesh.cheekatimala1@singtel.com>
2020-06-09 01:18:44 +0800

    initial commit for new address

---
+++ src/components/NewAddress/NewAddress.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NewAddress extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // @Hitesh write your source code here for NewAddress Component
+    };
+  }
+  // fill the below render method with form to save the address
+  render() {
+    return <h1>New NewAddress </h1>;
+  }
+}
+
+export default NewAddress;
