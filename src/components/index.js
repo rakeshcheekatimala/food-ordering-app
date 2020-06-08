9d58615a0b1867d8e82d14fe605797f26719e2fd
Rakesh Cheekatimala <rakesh.cheekatimala1@singtel.com>
2020-06-09 01:21:31 +0800

    adding the exports for newly added components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,6 @@ export { default as Header } from './Header';
 export { default as RestaurantCard } from './Card';
 export { default as ProfileDropdown } from './Dropdown';
 export { default as MessageSnackbar } from './Snackbar';
+export { default as AddressLayout } from './AddressLayout/AddressLayout';
+export { default as PaymentOptions } from './PaymentOptions/PaymentOptions';
+export { default as OrderSummary } from './OrderSummary/OrderSummary';
